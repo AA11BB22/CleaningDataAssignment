a4696b319df39ece8b56db2f17e1132b9db1b239
AA11BB22 <choechoonho@gmail.com>
2016-11-14 22:51:59 +0800

    Working first version

---
+++ README.md
@@ -0,0 +1,11 @@
+---
+title: "README"
+author: "Choe Choon Ho"
+date: "11/14/2016"
+---
+
+## Getting and Cleaning Data Assignment
+
+Run the run\_analysis.R script and it will download all necessary files.
+
+Disclaimer: an error will occur if there is a file named "UCI HAR Dataset" with different contents than what was expected in the assignment.
---
+++ run_analysis.R
@@ -0,0 +1,46 @@
+file <- "UCI HAR Dataset"
+
+if (!file.exists(file)){
+    url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+    download.file(url, file)
+    unzip(file)
+}
+
+# categories of activity (factors)
+activity <- read.table("UCI HAR Dataset/activity_labels.txt")
+activity <- as.vector(as.character(activity[, 2]))
+
+# features (columns)
+features <- read.table("UCI HAR Dataset/features.txt")
+features <- as.vector(as.character(features[, 2]))
+
+# training set
+train <- read.table("UCI HAR Dataset/train/X_train.txt")
+train[, ncol(train)+1] <- read.table("UCI HAR Dataset/train/Y_train.txt")
+train[, ncol(train)+1] <- read.table("UCI HAR Dataset/train/subject_train.txt")
+
+# test set
+test <- read.table("UCI HAR Dataset/test/X_test.txt")
+test[, ncol(test)+1] <- read.table("UCI HAR Dataset/test/Y_test.txt")
+test[, ncol(test)+1] <- read.table("UCI HAR Dataset/test/subject_test.txt")
+
+# merge both sets
+merged_data <- rbind(train, test)
+features <- c(features, "activity", "subject")
+colnames(merged_data) <- features
+
+# filter out columns
+filter <- grep("[Mm]ean[(][)]|[Ss]td[(][)]", features)
+filter <- c(filter, ncol(train)-1, ncol(train))
+
+filtered_data <- merged_data[, filter]
+
+# naming
+colnames(filtered_data) <- colnames(merged_data[, filter])
+filtered_data$activity <- factor(filtered_data$activity, levels = 1:length(activity), labels = activity)
+filtered_data$activity <- as.factor(filtered_data$activity)
+filtered_data$subject <- as.factor(filtered_data$subject)
+
+# group by activity and subject, then perform average on all columns except "activity" and "subject"
+tidy <- aggregate(filtered_data[-which(names(filtered_data) %in% c("activity", "subject"))], by=list(activity=filtered_data$activity, subject=filtered_data$subject), FUN=mean)
+
